93c8bb9673be821b232b5081954ef3c93366c894
Khottaewritecode <thamma4352@gmail.com>
2026-09-18 11:11:00 +0700

    2513110680 labsheet10

--- Labsheet10/BSTApp1.java
+++ Labsheet10/BSTApp1.java
@@ -1,23 +1,29 @@
-
 public class BSTApp1 {
 
 	public static void main(String[] args) {
-		BinarySearchTree tree =new BinarySearchTree();
+		
+		BinarySearchTree tree = new BinarySearchTree();
+		
 		tree.sampleTree1();
 		tree.printTree(tree.getRoot(), 0);
-		
 		System.out.println();
-		System.out.println("Minimum node is " + tree.findMinimum());
+		System.out.println("Minimum Node is " + tree.findMinimum());
+		System.out.println("Minimum Node is " + tree.findMaximum());
 		
-		System.out.println();
-		System.out.println("Maximum node is " + tree.findMaximum(tree.getRoot()));
+		System.out.println("Is 15 in BST => " + tree.findSpecificData(15));
 		
-		System.out.println();
-		int delNode = 60; //can change
+		int delNode = 60;
 		tree.searchDeleteNode(delNode);
-		System.out.println("Parent is "+tree.getParent().data);
-		System.out.println("Delete Node is "+tree.getDeleteNode().data);
+		System.out.println("Parent is " + tree.getParent().data);
+		System.out.println("Delete Node is " + tree.getDeleteNode().data);
+		
+		System.out.println();
+		tree.delete(delNode);
+		tree.printTree(tree.getRoot(), 0);
 		
 	}
-
+	
+	
+	
+	
 }
